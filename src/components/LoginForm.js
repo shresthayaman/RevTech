ccac4d65a4ec7f964f66e3ca898b48c4f2937e40
Nathan Park <nyp5aa@virginia.edu>
2018-06-12 16:35:04 -0400

    login form is working and saving typed email and password in state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
+import './LoginForm.css'
 import { Button } from 'antd';
 import { Input } from 'antd';
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: null,
+            password: null
+        }
+    }
+
+    updateInfo = (field, e) => {
+        this.setState({
+            [field]: e
+        });
+    }
+
     render() {
         return (
-            <div>
-                <div>
-                    email:
-                    <Input />
+            <div className="login-container">
+                <div className="input-fields">
+                    <Input
+                        placeholder="Email"
+                        onChange={(e) => this.updateInfo("email", e.target.value)}
+                        value={this.state.email}
+                    />
                 </div>
-                <div>
-                    password:
-                    <Input />
+                <div className="input-fields">
+                    <Input
+                        placeholder="Password"
+                        type="password" onChange={(e) => this.updateInfo("password", e.target.value)}
+                        value={this.state.password}
+                    />
                 </div>
-                <div>
-                    <Button> login </Button>
+                <div className="button-container">
+                    <Button>
+                        login
+                    </Button>
                 </div>
             </div>
         );
