34526e8d7cb5965a71df165583ef0132694bf3df
shresthayaman <shresthayaman@hotmail.com>
2018-06-13 15:12:40 -0400

    worked out some logic with storing link, about to merge with develop to obtain ascess to data base

--- src/components/ChallengeDisplay.js
+++ src/components/ChallengeDisplay.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Input } from "antd";
 
 import "./ChallengeDisplay.css";
+
+const LinkInput = Input.Search;
 
 export default class ChallenegeDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      submittedLink: ""
+    };
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.clickedChallenge !== nextProps.clickedChallenge) {
+      console.log("New prop recieved");
+      this.setState({
+        //set link to data in database
+        submittedLink: ""
+      });
+    }
+  }
+
+  handleLinkSubmit = link => {
+    this.setState({
+      submittedLink: link
+    });
+  };
+
   render() {
+    console.log(this.state.submittedLink);
     return (
       <div>
-        {console.log("I am in display!")}
-
         <Card className="card" title={this.props.clickedChallenge.title}>
           {this.props.clickedChallenge.description}akhdsfljahflkdfhakjsfh
           <br />
@@ -29,15 +48,18 @@ export default class ChallenegeDisplay extends React.Component {
           sdfasdfaasdfasfasdfasdfasdfasdf<br />
           asdfasdfasdfasdfadsfasdfadsf<br />
           sdfasdfaasdfasfasdfasdfasdfasdf<br />
-          asdfasdfasdfasdfadsfasdfadsf<br />
-          sdfasdfaasdfasfasdfasdfasdfasdf<br />
-          asdfasdfasdfasdfadsfasdfadsf<br />
-          sdfasdfaasdfasfasdfasdfasdfasdf<br />
-          asdfasdfasdfasdfadsfasdfadsf<br />
-          sdfasdfaasdfasfasdfasdfasdfasdf<br />
-          asdfasdfasdfasdfadsfasdfadsf<br />
-          sdfasdfaasdfasfasdfasdfasdfasdf<br />
         </Card>
+
+        <LinkInput
+          placeholder={
+            this.state.submittedLink === ""
+              ? "Input link to submit"
+              : this.state.submittedLink
+          }
+          enterButton="Submit"
+          size="default"
+          onSearch={link => this.handleLinkSubmit(link)}
+        />
       </div>
     );
   }
