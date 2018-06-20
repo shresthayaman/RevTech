2086f9162a147417bd9fa63e76ac7ec53388ab8c
shresthayaman <shresthayaman@hotmail.com>
2018-06-20 10:41:04 -0400

    Added admin daily challeneges to admin page

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
 import React from "react";
-import 'antd/dist/antd.css';
-import { Layout, Menu, Icon, Button } from 'antd';
-import InputDetails from './ContractInputDetails';
-import WebsiteUsers from './WebsiteUsers';
-import AdminMarket from './AdminMarket';
-import './WebsiteUsers.css';
+import "antd/dist/antd.css";
+import { Layout, Menu, Icon, Button } from "antd";
+import InputDetails from "./ContractInputDetails";
+import WebsiteUsers from "./WebsiteUsers";
+import AdminMarket from "./AdminMarket";
+import AdminDailyChallenge from "./AdminDailyChallenge";
+import "./WebsiteUsers.css";
 
 const { Header, Sider, Content } = Layout;
 
@@ -16,15 +17,14 @@ export default class SideBar extends React.Component {
     };
   }
 
-
-  handleMenuClick = (tab) => {
+  handleMenuClick = tab => {
     this.setState({
       page: tab
-    })
-  }
+    });
+  };
 
   state = {
-    collapsed: false,
+    collapsed: false
   };
 
   toggle = () => {
@@ -38,7 +38,7 @@ export default class SideBar extends React.Component {
       <Layout>
         <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
             <Menu.Item key="0" style={{ height: 130 }} selectable="false">
               <Icon type="" />
               <span>Admins Page</span>
@@ -47,15 +47,24 @@ export default class SideBar extends React.Component {
               <Icon type="user" />
               <span>Website Users</span>
             </Menu.Item>
-            <Menu.Item key="2" onClick={() => this.handleMenuClick("challenges")}>
+            <Menu.Item
+              key="2"
+              onClick={() => this.handleMenuClick("challenges")}
+            >
               <Icon type="form" />
               <span>Daily Challenges</span>
             </Menu.Item>
-            <Menu.Item key="3" onClick={() => this.handleMenuClick("pendingContracts")}>
+            <Menu.Item
+              key="3"
+              onClick={() => this.handleMenuClick("pendingContracts")}
+            >
               <Icon type="clock-circle-o" />
               <span>Pending Contracts</span>
             </Menu.Item>
-            <Menu.Item key="4" onClick={() => this.handleMenuClick("manageContracts")}>
+            <Menu.Item
+              key="4"
+              onClick={() => this.handleMenuClick("manageContracts")}
+            >
               <Icon type="idcard" />
               <span>Manage Contracts</span>
             </Menu.Item>
@@ -74,12 +83,27 @@ export default class SideBar extends React.Component {
           </Menu>
         </Sider>
         <Layout>
-          <Content style={{ margin: '155px 16px', padding: 24, background: '#fff', minHeight: 380, marginTop: 30 }}>
+          <Content
+            style={{
+              margin: "155px 16px",
+              padding: 24,
+              background: "#fff",
+              minHeight: 380,
+              marginTop: 30
+            }}
+          >
             {this.state.page === "" && <WebsiteUsers />}
             {this.state.page === "users" && <WebsiteUsers />}
-            {this.state.page === "challenges" && <div> challenges </div>}
-            {this.state.page === "pendingContracts" && <div> <AdminMarket /> </div>}
-            {this.state.page === "manageContracts" && <div> manage contract bids </div>}
+            {this.state.page === "challenges" && <AdminDailyChallenge />}
+            {this.state.page === "pendingContracts" && (
+              <div>
+                {" "}
+                <AdminMarket />{" "}
+              </div>
+            )}
+            {this.state.page === "manageContracts" && (
+              <div> manage contract bids </div>
+            )}
           </Content>
         </Layout>
       </Layout>
